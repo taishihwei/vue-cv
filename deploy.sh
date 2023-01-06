65f1d5159e2bd4d2e7186bca663b293624057252
Wesker <b511015@gmail.com>
2023-01-06 09:37:27 +0800

    Update deploy.sh
    .

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ set -e
 # 建立輸出檔案
 npm run build
 
-# 移動至到打包後的 dist 目錄 
+# 移動至打包後的 dist 目錄 
 cd dist
 
 # 因為 dist 資料夾預設是被 ignore 的，因此在進入 dist 資料夾後初始化 git
@@ -13,7 +13,7 @@ git add -A
 git commit -m 'deploy'
 
 # 將 dist 資料夾中的內容推送至遠端分支中，並強制無條件將舊有的內容取代成目前的內容（指令 git push -f)
-git push -f https://github.com/taishihwei/vue-cv.git master:gh-pages
+git push -f https://github.com/taishihwei/vue-cv.git main:gh-pages
 cd -
 
-# 執行指令，在終端機輸入 deploy.sh
+# 執行指令，在 Git bash 輸入 `sh deploy.sh`
